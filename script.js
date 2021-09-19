9bb40a5c330056a4de343b90c0ba208fddd67a1b
Vov4ik0191 <89548567+Vov4ik0191@users.noreply.github.com>
2021-09-19 19:39:17 +0300

    Final commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
-      $('.header__burger, .header__menu').toggleClass ('active')
+      $('.header__burger, .header__menu').toggleClass ('active');
+      $('body').toggleClass('lock'); // При вызове бургер-меню Добавляем к body class lock, для того что бы для класса Lock прописать запрет скролла страницы при открытом меню.
     })
 })
